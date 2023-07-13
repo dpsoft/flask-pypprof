0e61a125c1c65c09f27d5ee882e31433e9c58c7a
Diego <diegolparra@gmail.com>
2023-07-13 17:51:30 -0300

    add test

--- tests/profiing_test.py
+++ tests/profiing_test.py
@@ -1,18 +1,11 @@
 from flask_pypprof import get_pprof_blueprint
 from flask import Flask
-import pytest
-import os
 
 
-@pytest.fixture(scope='module')
 def test_pprof_endpoints():
-    # Set the Testing configuration prior to creating the Flask application
-    os.environ['CONFIG_TYPE'] = 'config.TestingConfig'
-
-    flask_app = Flask('flask_ping.flask_test_app')
-    # flask_app.register_blueprint(get_pprof_blueprint())
+    flask_app = Flask(__name__)
+    flask_app.register_blueprint(get_pprof_blueprint())
 
     with flask_app.test_client() as test_client:
-        response = test_client.get('/debug/pprof/profile')
-        assert response.status_code == 400
-        assert b"Flask User Management Example!" not in response.data
+        response = test_client.get('/debug/pprof/profile?seconds=1')
+        assert response.status_code == 200

40ad7b0e9a69bd7ed2123a669c35bb4ac9f576ae
Diego <diegolparra@gmail.com>
2023-07-13 17:00:19 -0300

    actions build

--- tests/profiing_test.py
+++ tests/profiing_test.py
@@ -1,11 +1,18 @@
-from flask import Flask
-import pytest
+# from flask import Flask
+# import pytest
 
-from flask_pypprof import get_pprof_blueprint
+# from flask_pypprof import get_pprof_blueprint
+
+from normalize import normalize
 
 
-@pytest.fixture(scope='module')
-def client():
-    app = Flask('flask_ping.flask_test_app')
-    app.register_blueprint(get_pprof_blueprint())
-    return app.test_client()
+def test_spaces():
+    number = '555 12 34' # input number
+    normalized_number = normalize(number)
+    assert normalized_number == '5551234'
+
+# @pytest.fixture(scope='module')
+# def client():
+#     app = Flask('flask_ping.flask_test_app')
+#     app.register_blueprint(get_pprof_blueprint())
+#     return app.test_client()

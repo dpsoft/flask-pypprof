bd9ceb3b9f279659eb7e59ed75f895dc6f6ed9b6
Diego <diegolparra@gmail.com>
2023-07-13 11:13:30 -0300

    actions publish

--- flask_pypprof/__init__.py
+++ flask_pypprof/__init__.py
@@ -4,3 +4,6 @@ Expose blueprint function
 from flask_pypprof.routes import get_pprof_endpoint_blueprint as blueprint
 
 get_pprof_blueprint = blueprint
+
+# Version of the flask_pypprof package
+__version__ = "0.1.0"

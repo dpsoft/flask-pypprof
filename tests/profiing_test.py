5f31f5db73d85516c2ec6efc490f41b01db3755c
Diego <diegolparra@gmail.com>
2023-07-14 10:23:00 -0300

    add test

--- tests/profiing_test.py
+++ tests/profiing_test.py
@@ -9,3 +9,16 @@ def test_pprof_endpoints():
     with flask_app.test_client() as test_client:
         response = test_client.get('/debug/pprof/profile?seconds=1')
         assert response.status_code == 200
+
+        response = test_client.get('/debug/pprof/thread')
+        assert response.status_code == 200
+
+        response = test_client.get('/debug/pprof/goroutine')
+        assert response.status_code == 200
+
+        response = test_client.get('/debug/pprof/heap')
+        assert response.status_code == 200
+
+        response = test_client.get('/debug/pprof/wall')
+        assert response.status_code == 200
+
